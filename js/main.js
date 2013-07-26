7a8b1ae8a4883a6ca6043da6f01a1efbfe9ca8f0
Hugo Forte <hugo@hugoforte.com>
2013-07-26 15:47:09 -0500

    Tag via javascript -> Analytics

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
-
+function HelloCtrl($scope) {
+    $scope.name = 'World';
+    $scope.AddGoogleTagEvent = function(){
+        alert('hi ' + $scope.name);
+        window.dataLayer.push({
+            'event': 'pageview',
+            'virtualUrl' : window.location.host + '/' + $scope.name
+        })
+    }
+}
